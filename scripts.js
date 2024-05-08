336c2961cb83d6fd52589af58877ecf77d083a7a
Man-Q <81616738+Man-Q@users.noreply.github.com>
2024-05-08 15:01:10 +0200

    Other changes.

--- scripts.js
+++ scripts.js
@@ -4,6 +4,22 @@ const result = document.querySelector("[data-result]");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
+
   const { dividend, divider } = Object.fromEntries(entries);
+
+  if (Number(dividend) === NaN || Number(divider) === NaN) {
+    throw "Something critical went wrong. Please reload the page";
+  }
+
+  if (dividend === "" || divider === "") {
+    result.innerText = `Division not performed. Both values are required in inputs. Try again`;
+    return;
+  }
+  
+  if (Number(divider) === 0) {
+    result.innerText = `Division not performed. Invalid number provided. Try again`;
+    console.trace("Cannot divide by zero.")
+    return;
+  }
   result.innerText = Math.floor(dividend / divider);
 });
